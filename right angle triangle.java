e727f932bc52966a05b4f3393283b07c26dfa551
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-08-21 09:06:59 +0530

    Create right angle triangle.java

---
+++ right angle triangle.java
@@ -0,0 +1,17 @@
+package pattern;
+import java.util.*;
+public class Pattern {
+	public static void main(String[] args) {
+		int n,i;
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the number");
+		n=input.nextInt();
+		for(i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
