3ee7ccf4c568ca8ce0a0b9f101da28eea41d76bc
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-08-21 09:34:30 +0530

    Update pattern for 1 01 101.java

--- pattern for 1 01 101.java
+++ pattern for 1 01 101.java
@@ -9,12 +9,7 @@ public class Pattern {
 		int num=1;
 		for(i=1;i<=n;i++) {
 			for(int j=1;j<=i;j++) {
-				if((i+j)%2==0) {
-					System.out.print("0");
-				}
-				else {
-					System.out.print("1");
-				}
+				System.out.print((i+j)%2 +" ");
 			}
 			System.out.println( " ");
 		}
