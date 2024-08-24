0a4cf96e153bc9f7affc7e39e908a39d1124fb16
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-08-24 14:31:43 +0530

    Create simple intrest.java

---
+++ simple intrest.java
@@ -0,0 +1,25 @@
+package reverseword;
+import java.util.*;
+public class Reverseword {
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter the amount");
+		int n=input.nextInt();
+		System.out.println("enter the time");
+		int time=input.nextInt();
+		input.nextLine();
+		System.out.println("senior citizen or not(y/n)");
+		String str=input.nextLine();
+		double intrest;
+		if(str.equals("y")) {
+			double r=10;
+		     intrest=(n*time*r)/100;
+		}
+		else {
+			double r1=12;
+		    intrest=(n*time*r1)/100;
+		}
+		
+		System.out.println("simple intrest is" + intrest);
+	}
+}
