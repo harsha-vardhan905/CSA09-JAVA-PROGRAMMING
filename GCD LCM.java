41e15d69f21b30835179e2d5a10289e09fe408bf
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-08-26 09:12:48 +0530

    Update and rename GCD.java to GCD LCM.java

--- GCD LCM.java
+++ GCD LCM.java
@@ -1,3 +1,4 @@
+package gcd;
 import java.util.Scanner;
 public class Gcd {
 	public static void main(String[] args) {
@@ -7,7 +8,9 @@ public class Gcd {
 		System.out.println("enter the sec no");
 		int n2=input.nextInt();
 		int gcd=findGCD(n1,n2);
+		int lcm=(n1*n2)/gcd;
 		System.out.println("gcd is"+gcd);
+		System.out.println("lcm is"+lcm);
 	}
 	public static int findGCD(int a,int b) {
 		if(b==0) {
