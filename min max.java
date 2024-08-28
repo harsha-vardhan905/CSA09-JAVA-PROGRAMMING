77f70cd95c17ebc2d6eaa56553cbfaf10a1fd725
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-08-28 09:33:40 +0530

    Create min max.java

---
+++ min max.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class MinMaxWithoutFunctions {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        // Bubble sort
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        // Find min and max
+        int min = array[0];
+        int max = array[array.length - 1];
+
+        System.out.println("Minimum Value: " + min);
+        System.out.println("Maximum Value: " + max);
+    }
+}
