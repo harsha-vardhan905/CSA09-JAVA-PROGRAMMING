7c5db8ef3f86058f96da44f13ece7d964042e926
harsha-vardhan905 <118434631+harsha-vardhan905@users.noreply.github.com>
2024-09-05 09:10:59 +0530

    Create int to bin and oct.java

---
+++ int to bin and oct.java
@@ -0,0 +1,9 @@
+public class Palindrome{
+    public static void main(String[] args){
+        int dec=15;
+        String s1=Integer.toBinaryString(dec);
+        String s2=Integer.toOctalString(dec);
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
